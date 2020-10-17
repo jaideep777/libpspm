c35748b4e7ded03438a905f1cde9814335b9e43a
Jaideep <jaideep777@gmail.com>
2020-10-17 18:10:35 +0200

    minor tweaks to time fmu/cm_u0in

--- tests/cm_u0in.cpp
+++ tests/cm_u0in.cpp
@@ -13,13 +13,13 @@ int main(){
 	Environment E;
 
 	Solver<TestModel,Environment> S(SOLVER_CM);
-	S.use_log_densities = false;
+	S.use_log_densities = true;
 	S.addSpecies(25, 0, 1, false, &M, {}, 2);
 	S.resetState();
 	S.initialize();
 	S.setEnvironment(&E);
 	S.get_species(0)->set_bfin_is_u0in(true);	// say that input_birth_flux is u0
-	S.print();
+	//S.print();
 	//for (auto s : S.state) cout << s << " "; cout << endl;
 
 	ofstream fout("cm_testmodel.txt");
@@ -29,7 +29,7 @@ int main(){
 	for (double t=0.05; t <= 8; t=t+0.05) {
 		S.step_to(t);
 		fout << S.current_time << "\t" << S.u0_out()[0] << "\t";
-		cout << S.current_time << "\t" << S.get_species(0)->size() << " " << S.u0_out()[0] << "\n";
+		//cout << S.current_time << "\t" << S.get_species(0)->size() << " " << S.u0_out()[0] << "\n";
 		//cout << S.u0_out() << "\n";
 		for (auto y : S.state) fout << y << "\t";
 		fout << endl;
@@ -37,6 +37,7 @@ int main(){
 
 	fout.close();
 
+	cout << S.u0_out()[0] << endl;
 	// test value is from R code	
 	if (abs(S.u0_out()[0] - 1.556967) < 1e-3) return 0;
 	else return 1;
--- tests/fmu_u0in.cpp
+++ tests/fmu_u0in.cpp
@@ -13,7 +13,6 @@ int main(){
 	Environment E;
 
 	Solver<TestModel,Environment> S(SOLVER_FMU);
-	S.control.ode_eps = 1e-4;
 	S.addSpecies(25, 0, 1, false, &M, {}, 2);
 	S.resetState();
 	S.initialize();
@@ -24,15 +23,15 @@ int main(){
 
 	for (double t=0.05; t <= 8; t=t+0.05) {
 		S.step_to(t);
-		fout << S.current_time << "\t" << 0/*S.u0_out()[0]*/ << "\t";
-		cout << S.current_time << " " /*<< S.u0_out()*/ << "\n";
+		fout << S.current_time << "\t" << S.u0_out()[0] << "\t";
+		//cout << S.current_time << " " [><< S.u0_out()<] << "\n";
 		for (auto y : S.state) fout << y << "\t";
 		fout << endl;
 	}
 
 	fout.close();
 
-	//cout <<S.u0_out()[0] << endl; 
+	cout << S.u0_out()[0] << endl; 
 	if (abs(S.u0_out()[0] - 1.468232) < 1e-5) return 0;
 	else return 1;
 
